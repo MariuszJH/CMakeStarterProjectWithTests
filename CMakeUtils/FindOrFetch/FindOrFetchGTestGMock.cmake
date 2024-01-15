b7b344e7ad8fb5fab390adbe70f096a4bf5cbdc9
MariuszJozef <mariusz.jozef.hoppe3@gmail.com>
2024-01-16 08:30:41 +1030

    GTest_DIR for Github actions 3a

--- CMakeUtils/FindOrFetch/FindOrFetchGTestGMock.cmake
+++ CMakeUtils/FindOrFetch/FindOrFetchGTestGMock.cmake
@@ -62,15 +62,21 @@ endif()
 if(WIN32 OR NOT ${packageName}_FOUND)
     include(FetchContent)
     set(FETCHCONTENT_QUIET FALSE)
+
+    if(NOT EXISTS ${CMAKE_SOURCE_DIR}/External/googletest)
     
-    FetchContent_Declare(googletest
-        GIT_REPOSITORY  https://github.com/google/googletest.git
-        GIT_TAG         main # v1.14.0
-        SOURCE_DIR      ${CMAKE_SOURCE_DIR}/External/googletest
-        GIT_PROGRESS    TRUE
-        GIT_SHALLOW     TRUE
-        USES_TERMINAL_DOWNLOAD TRUE   # <---- only used by Ninja generator
-    )
+        FetchContent_Declare(googletest
+            GIT_REPOSITORY  https://github.com/google/googletest.git
+            GIT_TAG         main # v1.14.0
+            SOURCE_DIR      ${CMAKE_SOURCE_DIR}/External/googletest
+            GIT_PROGRESS    TRUE
+            GIT_SHALLOW     TRUE
+            USES_TERMINAL_DOWNLOAD TRUE   # <---- only used by Ninja generator
+        )
+
+    else()
+        message(STATUS "googletest must have been fetched previously since ${CMAKE_SOURCE_DIR}/External/googletest already exists; not fetching googletest again!")
+    endif()
 
     FetchContent_MakeAvailable(googletest)
 endif()
